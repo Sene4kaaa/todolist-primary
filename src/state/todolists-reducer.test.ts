ffdefb508ac03c0a6269994871df5a2a463b8753
Sene4kaaa <senkevich92@mail.ru>
2023-06-07 21:44:31 +0300

    Refactoring code Add React.memo Add useCallback Todolist: refactoring useCallback: dependencies useCallback: EditableSpan Add component Task

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
     addTodolistAC,
     changeTodolistFilterAC,
@@ -9,15 +8,13 @@ import {
 import {v1} from 'uuid';
 import {FilterValuesType, TodolistType} from '../App';
 
-let todolistId1: string
-let todolistId2: string
-
-let startState: Array<TodolistType>
+let todolistId1: string;
+let todolistId2: string;
+let startState: Array<TodolistType> = [];
 
 beforeEach(() => {
     todolistId1 = v1();
     todolistId2 = v1();
-
     startState = [
         {id: todolistId1, title: "What to learn", filter: "all"},
         {id: todolistId2, title: "What to buy", filter: "all"}
@@ -39,12 +36,13 @@ test('correct todolist should be added', () => {
     expect(endState.length).toBe(3);
     expect(endState[0].title).toBe(newTodolistTitle);
     expect(endState[0].filter).toBe("all");
-    expect(endState[0].id).toBeDefined();
 });
 
 test('correct todolist should change its name', () => {
     let newTodolistTitle = "New Todolist";
+
     const action = changeTodolistTitleAC(todolistId2, newTodolistTitle);
+
     const endState = todolistsReducer(startState, action);
 
     expect(endState[0].title).toBe("What to learn");
@@ -53,12 +51,12 @@ test('correct todolist should change its name', () => {
 
 test('correct filter of todolist should be changed', () => {
     let newFilter: FilterValuesType = "completed";
+
     const action = changeTodolistFilterAC(todolistId2, newFilter);
+
     const endState = todolistsReducer(startState, action);
 
     expect(endState[0].filter).toBe("all");
     expect(endState[1].filter).toBe(newFilter);
 });
-
-
 
