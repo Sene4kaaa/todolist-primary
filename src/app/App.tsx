56c86ef319aa1ddb4ec97de7d30f4c5e8a32c2b4
Sene4kaaa <senkevich92@mail.ru>
2023-08-02 16:06:43 +0300

    Refactoring code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,15 +12,14 @@ import {
   Typography,
 } from "@mui/material";
 import { Menu } from "@mui/icons-material";
-import { initializeAppTC } from "app/app.reducer";
 import { Login } from "features/auth/Login";
-import { logoutTC } from "features/auth/auth.reducer";
 import "./App.css";
 import { TodolistsList } from "features/TodolistsList/TodolistsList";
 import { ErrorSnackbar } from "common/components";
 import { useAppDispatch } from "common/hooks";
 import { selectIsLoggedIn } from "features/auth/auth.selectors";
 import { selectAppStatus, selectIsInitialized } from "app/app.selectors";
+import {authThunks} from "../features/auth/auth.reducer";
 
 function App() {
   const status = useSelector(selectAppStatus);
@@ -30,47 +29,47 @@ function App() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(initializeAppTC());
+    dispatch( authThunks.initializeApp());
   }, []);
 
   const logoutHandler = useCallback(() => {
-    dispatch(logoutTC());
+    dispatch(authThunks.logout());
   }, []);
 
   if (!isInitialized) {
     return (
-      <div style={{ position: "fixed", top: "30%", textAlign: "center", width: "100%" }}>
-        <CircularProgress />
-      </div>
+        <div style={{ position: "fixed", top: "30%", textAlign: "center", width: "100%" }}>
+          <CircularProgress />
+        </div>
     );
   }
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <ErrorSnackbar />
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <Menu />
-            </IconButton>
-            <Typography variant="h6">News</Typography>
-            {isLoggedIn && (
-              <Button color="inherit" onClick={logoutHandler}>
-                Log out
-              </Button>
-            )}
-          </Toolbar>
-          {status === "loading" && <LinearProgress />}
-        </AppBar>
-        <Container fixed>
-          <Routes>
-            <Route path={"/"} element={<TodolistsList />} />
-            <Route path={"/login"} element={<Login />} />
-          </Routes>
-        </Container>
-      </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="App">
+          <ErrorSnackbar />
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" color="inherit" aria-label="menu">
+                <Menu />
+              </IconButton>
+              <Typography variant="h6">Sene4kaaa</Typography>
+              {isLoggedIn && (
+                  <Button color="inherit" onClick={logoutHandler}>
+                    Log out
+                  </Button>
+              )}
+            </Toolbar>
+            {status === "loading" && <LinearProgress />}
+          </AppBar>
+          <Container fixed>
+            <Routes>
+              <Route path={"/"} element={<TodolistsList />} />
+              <Route path={"/login"} element={<Login />} />
+            </Routes>
+          </Container>
+        </div>
+      </BrowserRouter>
   );
 }
 
--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -3,9 +3,9 @@ import { useFormik } from "formik";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField } from "@mui/material";
-import { loginTC } from "features/auth/auth.reducer";
 import { useAppDispatch } from "common/hooks";
 import { selectIsLoggedIn } from "features/auth/auth.selectors";
+import {authThunks} from "./auth.reducer";
 
 export const Login = () => {
   const dispatch = useAppDispatch();
@@ -31,7 +31,7 @@ export const Login = () => {
       rememberMe: false,
     },
     onSubmit: (values) => {
-      dispatch(loginTC(values));
+      dispatch(authThunks.login(values));
     },
   });
 
@@ -40,37 +40,37 @@ export const Login = () => {
   }
 
   return (
-    <Grid container justifyContent="center">
-      <Grid item xs={4}>
-        <form onSubmit={formik.handleSubmit}>
-          <FormControl>
-            <FormLabel>
-              <p>
-                To log in get registered{" "}
-                <a href={"https://social-network.samuraijs.com/"} target={"_blank"}>
-                  here
-                </a>
-              </p>
-              <p>or use common test account credentials:</p>
-              <p> Email: free@samuraijs.com</p>
-              <p>Password: free</p>
-            </FormLabel>
-            <FormGroup>
-              <TextField label="Email" margin="normal" {...formik.getFieldProps("email")} />
-              {formik.errors.email ? <div>{formik.errors.email}</div> : null}
-              <TextField type="password" label="Password" margin="normal" {...formik.getFieldProps("password")} />
-              {formik.errors.password ? <div>{formik.errors.password}</div> : null}
-              <FormControlLabel
-                label={"Remember me"}
-                control={<Checkbox {...formik.getFieldProps("rememberMe")} checked={formik.values.rememberMe} />}
-              />
-              <Button type={"submit"} variant={"contained"} color={"primary"}>
-                Login
-              </Button>
-            </FormGroup>
-          </FormControl>
-        </form>
+      <Grid container justifyContent="center">
+        <Grid item xs={4}>
+          <form onSubmit={formik.handleSubmit}>
+            <FormControl>
+              <FormLabel>
+                <p>
+                  To log in get registered{" "}
+                  <a href={"https://social-network.samuraijs.com/"} target={"_blank"}>
+                    here
+                  </a>
+                </p>
+                <p>or use common test account credentials:</p>
+                <p> Email: free@samuraijs.com</p>
+                <p>Password: free</p>
+              </FormLabel>
+              <FormGroup>
+                <TextField label="Email" margin="normal" {...formik.getFieldProps("email")} />
+                {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                <TextField type="password" label="Password" margin="normal" {...formik.getFieldProps("password")} />
+                {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                <FormControlLabel
+                    label={"Remember me"}
+                    control={<Checkbox {...formik.getFieldProps("rememberMe")} checked={formik.values.rememberMe} />}
+                />
+                <Button type={"submit"} variant={"contained"} color={"primary"}>
+                  Login
+                </Button>
+              </FormGroup>
+            </FormControl>
+          </form>
+        </Grid>
       </Grid>
-    </Grid>
   );
 };
