9d07f3bb7769fa89dacb24a810fe55e6778070b2
Sene4kaaa <senkevich92@mail.ru>
2023-08-02 18:18:57 +0300

    Refactoring code Add jsdoc in handle-server-app-error.ts

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -2,6 +2,13 @@ import {Dispatch} from "redux";
 import {appActions} from "app/app.reducer";
 import {ResponseType} from "common/types/common.types";
 
+/**
+ *
+ * @param data
+ * @param dispatch
+ * @param showError
+ */
+
 export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch, showError: boolean = true) => {
     if (showError) {
         dispatch(appActions.setAppError({error: data.messages.length ? data.messages[0] : "Some error occurred"}));
