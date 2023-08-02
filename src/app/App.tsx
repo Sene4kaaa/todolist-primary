27d86e6d6c1eac0e58d9e595868f3355aebc74f2
Sene4kaaa <senkevich92@mail.ru>
2023-08-02 19:37:01 +0300

    Refactoring code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,79 +1,80 @@
-import React, { useCallback, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, {useCallback, useEffect} from "react";
+import {useSelector} from "react-redux";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {
-  AppBar,
-  Button,
-  CircularProgress,
-  Container,
-  IconButton,
-  LinearProgress,
-  Toolbar,
-  Typography,
+    AppBar,
+    Button,
+    CircularProgress,
+    Container,
+    IconButton,
+    LinearProgress,
+    Toolbar,
+    Typography,
 } from "@mui/material";
-import { Menu } from "@mui/icons-material";
-import { Login } from "features/auth/Login";
+import {Menu} from "@mui/icons-material";
+import {Login} from "features/auth/Login";
 import "./App.css";
-import { TodolistsList } from "features/TodolistsList/TodolistsList";
-import { ErrorSnackbar } from "common/components";
-import { useAppDispatch } from "common/hooks";
-import { selectIsLoggedIn } from "features/auth/auth.selectors";
-import { selectAppStatus, selectIsInitialized } from "app/app.selectors";
+import {TodolistsList} from "features/TodolistsList/TodolistsList";
+import {ErrorSnackbar} from "common/components";
+import {useActions, useAppDispatch} from "common/hooks";
+import {selectIsLoggedIn} from "features/auth/auth.selectors";
+import {selectAppStatus, selectIsInitialized} from "app/app.selectors";
 import {authThunks} from "features/auth/auth.reducer";
 import {bindActionCreators} from "redux";
 
 function App() {
-  const status = useSelector(selectAppStatus);
-  const isInitialized = useSelector(selectIsInitialized);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+    const status = useSelector(selectAppStatus);
+    const isInitialized = useSelector(selectIsInitialized);
+    const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  const dispatch = useAppDispatch();
+    const {initializeApp, logout} = useActions(authThunks);
 
-  useEffect(() => {
-    // dispatch( authThunks.initializeApp());
-    bindActionCreators(authThunks.initializeApp, dispatch)()
+    useEffect(() => {
+        // dispatch( authThunks.initializeApp());
+        initializeApp()
 
-  }, []);
+    }, []);
 
-  const logoutHandler = useCallback(() => {
-    dispatch(authThunks.logout());
-  }, []);
+    const logoutHandler = useCallback(() => {
+        // dispatch(authThunks.logout());
+        logout()
+    }, []);
 
-  if (!isInitialized) {
+    if (!isInitialized) {
+        return (
+            <div style={{position: "fixed", top: "30%", textAlign: "center", width: "100%"}}>
+                <CircularProgress/>
+            </div>
+        );
+    }
+
     return (
-        <div style={{ position: "fixed", top: "30%", textAlign: "center", width: "100%" }}>
-          <CircularProgress />
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <ErrorSnackbar/>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <Menu/>
+                        </IconButton>
+                        <Typography variant="h6">Sene4kaaa</Typography>
+                        {isLoggedIn && (
+                            <Button color="inherit" onClick={logoutHandler}>
+                                Log out
+                            </Button>
+                        )}
+                    </Toolbar>
+                    {status === "loading" && <LinearProgress/>}
+                </AppBar>
+                <Container fixed>
+                    <Routes>
+                        <Route path={"/"} element={<TodolistsList/>}/>
+                        <Route path={"/login"} element={<Login/>}/>
+                    </Routes>
+                </Container>
+            </div>
+        </BrowserRouter>
     );
-  }
-
-  return (
-      <BrowserRouter>
-        <div className="App">
-          <ErrorSnackbar />
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton edge="start" color="inherit" aria-label="menu">
-                <Menu />
-              </IconButton>
-              <Typography variant="h6">Sene4kaaa</Typography>
-              {isLoggedIn && (
-                  <Button color="inherit" onClick={logoutHandler}>
-                    Log out
-                  </Button>
-              )}
-            </Toolbar>
-            {status === "loading" && <LinearProgress />}
-          </AppBar>
-          <Container fixed>
-            <Routes>
-              <Route path={"/"} element={<TodolistsList />} />
-              <Route path={"/login"} element={<Login />} />
-            </Routes>
-          </Container>
-        </div>
-      </BrowserRouter>
-  );
 }
 
 export default App;
