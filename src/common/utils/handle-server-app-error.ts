1728062ad6fcd897fde1559e6af312fccf09aa01
Sene4kaaa <senkevich92@mail.ru>
2023-08-02 18:22:39 +0300

    Refactoring code Add jsdoc in handle-server-app-error.ts

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -3,10 +3,10 @@ import {appActions} from "app/app.reducer";
 import {ResponseType} from "common/types/common.types";
 
 /**
- *
- * @param data
- * @param dispatch
- * @param showError
+ * Данная функция обрабатывает ошибки, которые могут возникнуть при взаимодействии с сервером.
+ * @param data  - ответ от сервера в формате ResponseType<D>
+ * @param dispatch - функция для отправки сообщений в store Redux
+ * @param showError - флаг, указывающий, нужно ли отображать ошибки в пользовательском интерфейсе
  */
 
 export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch, showError: boolean = true) => {
