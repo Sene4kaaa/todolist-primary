274852405d9f3bb1bc6af2ee433a9f71e4a576fc
Sene4kaaa <senkevich92@mail.ru>
2023-08-02 19:05:37 +0300

    Refactoring code

--- src/common/utils/thunk-try-catch.ts
+++ src/common/utils/thunk-try-catch.ts
@@ -1,21 +1,21 @@
-import { AppDispatch, AppRootStateType } from 'app/store';
-import { handleServerNetworkError } from 'common/utils/handle-server-network-error';
-import { BaseThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk';
-import { appActions } from 'app/app.reducer';
-import { ResponseType } from 'common/types';
+import {AppDispatch, AppRootStateType} from 'app/store';
+import {handleServerNetworkError} from 'common/utils/handle-server-network-error';
+import {BaseThunkAPI} from '@reduxjs/toolkit/dist/createAsyncThunk';
+import {appActions} from 'app/app.reducer';
+import {ResponseType} from 'common/types';
 
-export const thunkTryCatch = async <T>(
+export const thunkTryCatch = async (
     thunkAPI: BaseThunkAPI<AppRootStateType, any, AppDispatch, null | ResponseType>,
-    logic: () => Promise<T>
-): Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>> => {
-    const { dispatch, rejectWithValue } = thunkAPI;
-    dispatch(appActions.setAppStatus({ status: "loading" }));
+    logic: Function,
+) => {
+    const {dispatch, rejectWithValue} = thunkAPI;
+    dispatch(appActions.setAppStatus({status: "loading"}));
     try {
         return await logic();
     } catch (e) {
         handleServerNetworkError(e, dispatch);
         return rejectWithValue(null);
     } finally {
-        dispatch(appActions.setAppStatus({ status: "idle" }));
+        dispatch(appActions.setAppStatus({status: "idle"}));
     }
 };
