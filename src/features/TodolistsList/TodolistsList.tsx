815332df91a10ee684565f70dccd3bf314b60ca8
Sene4kaaa <senkevich92@mail.ru>
2023-08-02 19:54:37 +0300

    Refactoring code

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -1,98 +1,117 @@
-import React, { useCallback, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { FilterValuesType, todolistsActions, todolistsThunks } from "features/TodolistsList/todolists.reducer";
-import { tasksThunks } from "features/TodolistsList/tasks.reducer";
-import { Grid, Paper } from "@mui/material";
-import { AddItemForm } from "common/components";
-import { Todolist } from "./Todolist/Todolist";
-import { Navigate } from "react-router-dom";
-import { useAppDispatch } from "common/hooks";
-import { selectIsLoggedIn } from "features/auth/auth.selectors";
-import { selectTasks } from "features/TodolistsList/tasks.selectors";
-import { selectTodolists } from "features/TodolistsList/todolists.selectors";
-import { TaskStatuses } from "common/enums";
+import React, {useCallback, useEffect} from "react";
+import {useSelector} from "react-redux";
+import {FilterValuesType, todolistsActions, todolistsThunks} from "features/TodolistsList/todolists.reducer";
+import {tasksThunks} from "features/TodolistsList/tasks.reducer";
+import {Grid, Paper} from "@mui/material";
+import {AddItemForm} from "common/components";
+import {Todolist} from "./Todolist/Todolist";
+import {Navigate} from "react-router-dom";
+import {useActions, useAppDispatch} from "common/hooks";
+import {selectIsLoggedIn} from "features/auth/auth.selectors";
+import {selectTasks} from "features/TodolistsList/tasks.selectors";
+import {selectTodolists} from "features/TodolistsList/todolists.selectors";
+import {TaskStatuses} from "common/enums";
 
 export const TodolistsList = () => {
-  const todolists = useSelector(selectTodolists);
-  const tasks = useSelector(selectTasks);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+    const todolists = useSelector(selectTodolists);
+    const tasks = useSelector(selectTasks);
+    const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  const dispatch = useAppDispatch();
+    const {
+        fetchTodolists,
+        removeTodolist: removeTodolistThunk,
+        changeTodolistTitle: changeTodolistTitleThunk,
+        addTodolist: addTodolistThunk
+    } = useActions(todolistsThunks);
 
-  useEffect(() => {
+    const {changeTodolistFilter} = useActions(todolistsActions)
+
+    const {removeTask: removeTaskThunk, addTask: addTaskThunk, updateTask: updateTaskThunk} = useActions(tasksThunks)
+
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            return;
+        }
+        // dispatch(todolistsThunks.fetchTodolists());
+        fetchTodolists()
+    }, []);
+
+    const removeTask = useCallback(function (taskId: string, todolistId: string) {
+        // dispatch(tasksThunks.removeTask({taskId, todolistId}));
+        removeTaskThunk({taskId, todolistId})
+    }, []);
+
+    const addTask = useCallback(function (title: string, todolistId: string) {
+        // dispatch(tasksThunks.addTask({title, todolistId}));
+        addTaskThunk({title, todolistId})
+    }, []);
+
+    const changeStatus = useCallback(function (taskId: string, status: TaskStatuses, todolistId: string) {
+        // dispatch(tasksThunks.updateTask({taskId, domainModel: {status}, todolistId}));
+        updateTaskThunk({taskId, domainModel: {status}, todolistId})
+    }, []);
+
+    const changeTaskTitle = useCallback(function (taskId: string, title: string, todolistId: string) {
+        // dispatch(tasksThunks.updateTask({taskId, domainModel: {title}, todolistId}));
+        updateTaskThunk({taskId, domainModel: {title}, todolistId})
+    }, []);
+
+    const changeFilter = useCallback(function (filter: FilterValuesType, id: string) {
+        // dispatch(todolistsActions.changeTodolistFilter({id, filter}));
+        changeTodolistFilter({id, filter})
+    }, []);
+
+    const removeTodolist = useCallback(function (id: string) {
+        // dispatch(todolistsThunks.removeTodolist(id));
+        removeTodolistThunk(id)
+    }, []);
+
+    const changeTodolistTitle = useCallback(function (id: string, title: string) {
+        // dispatch(todolistsThunks.changeTodolistTitle({ id, title }));
+        changeTodolistTitleThunk({id, title})
+    }, []);
+
+    const addTodolist = useCallback(
+        (title: string) => {
+            // dispatch(todolistsThunks.addTodolist(title));
+            addTodolistThunk(title)
+        },
+        [],
+    );
+
     if (!isLoggedIn) {
-      return;
+        return <Navigate to={"/login"}/>;
     }
-    dispatch(todolistsThunks.fetchTodolists());
-  }, []);
 
-  const removeTask = useCallback(function (taskId: string, todolistId: string) {
-    dispatch(tasksThunks.removeTask({ taskId, todolistId }));
-  }, []);
-
-  const addTask = useCallback(function (title: string, todolistId: string) {
-    dispatch(tasksThunks.addTask({ title, todolistId }));
-  }, []);
-
-  const changeStatus = useCallback(function (taskId: string, status: TaskStatuses, todolistId: string) {
-    dispatch(tasksThunks.updateTask({ taskId, domainModel: { status }, todolistId }));
-  }, []);
-
-  const changeTaskTitle = useCallback(function (taskId: string, title: string, todolistId: string) {
-    dispatch(tasksThunks.updateTask({ taskId, domainModel: { title }, todolistId }));
-  }, []);
-
-  const changeFilter = useCallback(function (filter: FilterValuesType, id: string) {
-    dispatch(todolistsActions.changeTodolistFilter({ id, filter }));
-  }, []);
-
-  const removeTodolist = useCallback(function (id: string) {
-    dispatch(todolistsThunks.removeTodolist(id));
-  }, []);
-
-  const changeTodolistTitle = useCallback(function (id: string, title: string) {
-    dispatch(todolistsThunks.changeTodolistTitle({ id, title }));
-  }, []);
-
-  const addTodolist = useCallback(
-    (title: string) => {
-      dispatch(todolistsThunks.addTodolist(title));
-    },
-    [dispatch],
-  );
-
-  if (!isLoggedIn) {
-    return <Navigate to={"/login"} />;
-  }
-
-  return (
-    <>
-      <Grid container style={{ padding: "20px" }}>
-        <AddItemForm addItem={addTodolist} />
-      </Grid>
-      <Grid container spacing={3}>
-        {todolists.map((tl) => {
-          let allTodolistTasks = tasks[tl.id];
-
-          return (
-            <Grid item key={tl.id}>
-              <Paper style={{ padding: "10px" }}>
-                <Todolist
-                  todolist={tl}
-                  tasks={allTodolistTasks}
-                  removeTask={removeTask}
-                  changeFilter={changeFilter}
-                  addTask={addTask}
-                  changeTaskStatus={changeStatus}
-                  removeTodolist={removeTodolist}
-                  changeTaskTitle={changeTaskTitle}
-                  changeTodolistTitle={changeTodolistTitle}
-                />
-              </Paper>
+    return (
+        <>
+            <Grid container style={{padding: "20px"}}>
+                <AddItemForm addItem={addTodolist}/>
             </Grid>
-          );
-        })}
-      </Grid>
-    </>
-  );
+            <Grid container spacing={3}>
+                {todolists.map((tl) => {
+                    let allTodolistTasks = tasks[tl.id];
+
+                    return (
+                        <Grid item key={tl.id}>
+                            <Paper style={{padding: "10px"}}>
+                                <Todolist
+                                    todolist={tl}
+                                    tasks={allTodolistTasks}
+                                    removeTask={removeTask}
+                                    changeFilter={changeFilter}
+                                    addTask={addTask}
+                                    changeTaskStatus={changeStatus}
+                                    removeTodolist={removeTodolist}
+                                    changeTaskTitle={changeTaskTitle}
+                                    changeTodolistTitle={changeTodolistTitle}
+                                />
+                            </Paper>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </>
+    );
 };
