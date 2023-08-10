ecba54781e92775bd23deb8e1d85cad9b57b7feb
Sene4kaaa <senkevich92@mail.ru>
2023-08-10 19:41:35 +0300

    Refactoring code

--- src/features/todolists-list/todolists/todolist/Todolist.tsx
+++ src/features/todolists-list/todolists/todolist/Todolist.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from "react";
 import { Delete } from "@mui/icons-material";
 import { Button, IconButton } from "@mui/material";
-import { Task } from "../../tasks/ui/task/Task";
+import { Task } from "features/todolists-list/tasks/ui/task/task";
 import { FilterValuesType, TodolistDomainType } from "features/todolists-list/todolists/todolists.reducer";
 import { tasksThunks } from "features/todolists-list/tasks/model/tasks.reducer";
 import { TaskStatuses } from "common/enums";
