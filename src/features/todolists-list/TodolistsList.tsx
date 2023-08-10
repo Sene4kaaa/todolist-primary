cc5ebbe776784b098660df30a03bb628b3b202f8
Sene4kaaa <senkevich92@mail.ru>
2023-08-10 19:50:37 +0300

    Refactoring code

--- src/features/todolists-list/TodolistsList.tsx
+++ src/features/todolists-list/TodolistsList.tsx
@@ -24,7 +24,7 @@ export const TodolistsList = () => {
     changeTodolistTitle: changeTodolistTitleThunk,
   } = useActions(todolistsThunks);
 
-  const { addTask: addTaskThunk} = useActions(tasksThunks);
+
   const { changeTodolistFilter } = useActions(todolistsActions);
 
   useEffect(() => {
@@ -33,14 +33,6 @@ export const TodolistsList = () => {
     }
     fetchTodolists();
   }, []);
-
-
-
-  const addTask = useCallback(function (title: string, todolistId: string) {
-    addTaskThunk({ title, todolistId });
-  }, []);
-
-
 
   const changeFilter = useCallback(function (filter: FilterValuesType, id: string) {
     changeTodolistFilter({ id, filter });
@@ -78,7 +70,6 @@ export const TodolistsList = () => {
                   todolist={tl}
                   tasks={allTodolistTasks}
                   changeFilter={changeFilter}
-                  addTask={addTask}
                   removeTodolist={removeTodolist}
                   changeTodolistTitle={changeTodolistTitle}
                 />
