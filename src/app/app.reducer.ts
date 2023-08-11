006c8336c088a672f468cc490c88c0bc9d30a21e
Sene4kaaa <senkevich92@mail.ru>
2023-08-11 14:13:00 +0300

    Refactoring code

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,37 +1,37 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 const initialState = {
-  status: "idle" as RequestStatusType,
-  error: null as string | null,
-  isInitialized: false,
+    status: "idle" as RequestStatusType,
+    error: null as string | null,
+    isInitialized: false,
 };
 
 export type AppInitialStateType = typeof initialState;
 export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed";
 
 const slice = createSlice({
-  name: "app",
-  initialState,
-  reducers: {
-    setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
-      state.error = action.payload.error;
-    },
-    setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
-      state.status = action.payload.status;
-    },
-    setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
-      state.isInitialized = action.payload.isInitialized;
-    },
-  },
-  extraReducers: (builder)=>{
-    builder.addMatcher(
-        (action)=>{
-          return true
+    name: "app",
+    initialState,
+    reducers: {
+        setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
+            state.error = action.payload.error;
         },
-        (state, action) => {
-
-    })
-  }
+        setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+            state.status = action.payload.status;
+        },
+        setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
+            state.isInitialized = action.payload.isInitialized;
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addMatcher(
+            (action) => {
+                return action.type.endsWith("/pending")
+            },
+            (state) => {
+                state.status = "loading"
+            })
+    }
 });
 
 export const appReducer = slice.reducer;
