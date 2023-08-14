76b283c43c21b9c5e9c57619afb8a61873fdf362
Sene4kaaa <senkevich92@mail.ru>
2023-08-14 16:34:21 +0300

    Refactoring code

--- src/features/todolists-list/tasks/model/tasks.reducer.ts
+++ src/features/todolists-list/tasks/model/tasks.reducer.ts
@@ -45,7 +45,7 @@ const updateTask = createAppAsyncThunk<UpdateTaskArgType, UpdateTaskArgType>(
             const state = getState();
             const task = state.tasks[arg.todolistId].find((t) => t.id === arg.taskId);
             if (!task) {
-                dispatch(appActions.setAppError({error: "Task not found in the state"}));
+                dispatch(appActions.setAppError({error: "task not found in the state"}));
                 return rejectWithValue(null);
             }
 
