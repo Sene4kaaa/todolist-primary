96e2e605234b778a814f7db733a151b2a999d810
Sene4kaaa <senkevich92@mail.ru>
2023-08-14 18:08:29 +0300

    Refactoring code

---
+++ src/features/todolists-list/todolists/ui/todolist/todolist-title/todolist-title.tsx
@@ -0,0 +1,35 @@
+import {EditableSpan} from "common/components";
+import {IconButton} from "@mui/material";
+import {Delete} from "@mui/icons-material";
+import React, {FC, useCallback} from "react";
+import {useActions} from "common/hooks";
+import {TodolistDomainType, todolistsThunks} from "features/todolists-list/todolists/model/todolists.reducer";
+
+type Props = {
+    todolist: TodolistDomainType
+}
+
+export const TodolistTitle: FC<Props> = ({todolist}) => {
+
+    const {removeTodolist, changeTodolistTitle,} = useActions(todolistsThunks);
+
+    const removeTodolistHandler = () => {
+        removeTodolist(todolist.id);
+    };
+
+    const changeTodolistTitleCallback = useCallback(
+        (title: string) => {
+            changeTodolistTitle({id: todolist.id, title});
+        },
+        [todolist.id],
+    );
+
+    return (
+        <h3>
+            <EditableSpan value={todolist.title} onChange={changeTodolistTitleCallback}/>
+            <IconButton onClick={removeTodolistHandler} disabled={todolist.entityStatus === "loading"}>
+                <Delete/>
+            </IconButton>
+        </h3>
+    )
+}
--- src/features/todolists-list/todolists/ui/todolist/todolist.tsx
+++ src/features/todolists-list/todolists/ui/todolist/todolist.tsx
@@ -1,15 +1,14 @@
 import React, {memo, useCallback, useEffect} from "react";
-import {Delete} from "@mui/icons-material";
-import {IconButton} from "@mui/material";
-import {TodolistDomainType, todolistsThunks} from "features/todolists-list/todolists/model/todolists.reducer";
+import {TodolistDomainType} from "features/todolists-list/todolists/model/todolists.reducer";
 import {tasksThunks} from "features/todolists-list/tasks/model/tasks.reducer";
 import {useActions} from "common/hooks";
-import {AddItemForm, EditableSpan} from "common/components";
+import {AddItemForm} from "common/components";
 import {TaskType} from "features/todolists-list/tasks/api/tasks.api.types";
 import {
     FilterTasksButtons
 } from "features/todolists-list/todolists/ui/todolist/filter-tasks-buttons/filter-tasks-buttons";
 import {Tasks} from "features/todolists-list/todolists/ui/todolist/tasks/tasks";
+import {TodolistTitle} from "features/todolists-list/todolists/ui/todolist/todolist-title/todolist-title";
 
 type Props = {
     todolist: TodolistDomainType;
@@ -18,9 +17,6 @@ type Props = {
 
 export const Todolist = memo((props: Props) => {
     const {fetchTasks, addTask} = useActions(tasksThunks);
-
-    const {removeTodolist, changeTodolistTitle,} = useActions(todolistsThunks);
-
 
     useEffect(() => {
         fetchTasks(props.todolist.id);
@@ -33,25 +29,9 @@ export const Todolist = memo((props: Props) => {
         [props.todolist.id],
     );
 
-    const removeTodolistHandler = () => {
-        removeTodolist(props.todolist.id);
-    };
-
-    const changeTodolistTitleCallback = useCallback(
-        (title: string) => {
-            changeTodolistTitle({id: props.todolist.id, title});
-        },
-        [props.todolist.id],
-    );
-
     return (
         <div>
-            <h3>
-                <EditableSpan value={props.todolist.title} onChange={changeTodolistTitleCallback}/>
-                <IconButton onClick={removeTodolistHandler} disabled={props.todolist.entityStatus === "loading"}>
-                    <Delete/>
-                </IconButton>
-            </h3>
+            <TodolistTitle todolist={props.todolist}/>
             <AddItemForm addItem={addTaskCallback} disabled={props.todolist.entityStatus === "loading"}/>
             <Tasks todolist={props.todolist} tasks={props.tasks}/>
             <div style={{paddingTop: "10px"}}>
