69f2691e07dfe622a16f6f5fb76eb90754ff4525
Sene4kaaa <senkevich92@mail.ru>
2023-08-14 20:15:26 +0300

    Refactoring code

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,3 +1,3 @@
-export { AddItemForm } from "./AddItemForm/AddItemForm";
-export { EditableSpan } from "./EditableSpan/EditableSpan";
-export { ErrorSnackbar } from "./ErrorSnackbar/ErrorSnackbar";
+export { AddItemForm } from "common/components/add-item-form/add-item-form";
+export { EditableSpan } from "common/components/editable-span/editable-span";
+export { ErrorSnackbar } from "common/components/error-snackbar/error-snackbar";
